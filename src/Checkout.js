a8dbe165b52475122551bf4b4c8b4826bc0d0e12
Muskanpatel26 <patelmuskan417@gmail.com>
2022-01-16 20:36:46 +0530

    16jan

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,14 +5,16 @@ import Subtotal from './Subtotal';
 import { useStateValue } from './ContextApi';
 
 function Checkout() {
-    const [{cart},dispatch] = useStateValue()
+    const [{cart, user}, dispatch] = useStateValue(); 
+  
     return (
-        <div className="checkout">
-            <div className="checkout__left">
-                <img className="checkout__adv" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/Jupiter21P1/pay_stripe_desk.png" alt="adv" />
-                <div>
-                    <h3 className="checkout__title">Your Shopping Details</h3>
-                    {/* cart item here */}
+      <div className="checkout">
+        <div className="checkout__left">
+            <img className="checkout__adv" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/Jupiter21P1/pay_stripe_desk.png" alt="adv" />
+          <div>
+            <h3>{user?.email}</h3>
+            <h3 className="checkout__title">Your Shopping Details</h3>
+            
             {cart.map( item => (
              <CheckoutProduct 
               id = {item.id}
@@ -21,19 +23,14 @@ function Checkout() {
               price = {item.price}
              /> 
             ))}
-                    {/* cart item here */}
-                </div>
-
-            </div>
-
-            <div className="checkout__right">
-               <Subtotal />
-                    
-               
-            </div>
-            
+          </div>
         </div>
+  
+        <div className="checkout__right">
+          <Subtotal />
+        </div>
+      </div>
     );
-}
-
-export default Checkout;
+  }
+  
+  export default Checkout;
